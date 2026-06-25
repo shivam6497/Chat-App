453734e9559a83744358965c8e99f66783a6e9b7
shivam6497 <kdmboy6397@gmail.com>
2026-06-25 17:56:34 +0000

    Added some room logic

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,34 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
+interface User {
+    socket: WebSocket,
+    room: string
+}
 
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-    console.log("connected");
+    
+    socket.on("message", (message) => {
+        const parsedMessage = JSON.parse(message as unknown as string);
 
-    socket.on("message", (e) => {
-        if(e.toString() === "ping") {
-            socket.send("pong");
+        if(parsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
         }
-    });
+
+        if(parsedMessage.type === "chat") {
+            const currentUserRoom = allSockets.find(user => user.socket === socket)?.room;
+            if (currentUserRoom === undefined) return;
+
+            for(let i = 0;i < allSockets.length;i++) {
+                if(allSockets[i]?.room === currentUserRoom) {
+                    allSockets[i]?.socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
+    })
 })
