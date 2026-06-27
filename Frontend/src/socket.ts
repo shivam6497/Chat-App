ad675cbf7c7da648156a14947f6f0b798dc61bb3
shivam6497 <kdmboy6397@gmail.com>
2026-06-27 17:49:50 +0000

    Tested the Backend

---
+++ Frontend/src/socket.ts
@@ -0,0 +1,11 @@
+import { io, Socket } from 'socket.io-client';
+import type { ClientToServerEvent, ServerToClientEvent } from './types';
+ 
+const SERVER_URL = 'http://localhost:8080';
+ 
+export const socket: Socket<ServerToClientEvent, ClientToServerEvent> = io(
+  SERVER_URL,
+  {
+    autoConnect: true,
+  }
+);
